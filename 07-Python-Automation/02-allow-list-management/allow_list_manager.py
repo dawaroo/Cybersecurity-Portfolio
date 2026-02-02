cd5bbbbe8d21e88adcaddf3455d634581c14153c
Daniel waser C security <waserdaniel2000@gmail.com>
2026-02-02 14:02:15 -0600

    Update allow_list_manager.py

--- 07-Python-Automation/02-allow-list-management/allow_list_manager.py
+++ 07-Python-Automation/02-allow-list-management/allow_list_manager.py
@@ -1,1 +1,29 @@
-ssadsadsadsa
+# Allow List Management Script
+
+import_file = "allow_list.txt"
+
+remove_list = [
+    "192.168.97.225",
+    "192.168.158.170",
+    "192.168.201.40",
+    "192.168.58.57"
+]
+
+# Read the allow list from file
+with open(import_file, "r") as file:
+    ip_addresses = file.read()
+
+# Convert string to list
+ip_addresses = ip_addresses.split()
+
+# Remove unauthorized IP addresses
+for ip in ip_addresses:
+    if ip in remove_list:
+        ip_addresses.remove(ip)
+
+# Convert list back to string
+updated_allow_list = " ".join(ip_addresses)
+
+# Rewrite the allow list file
+with open(import_file, "w") as file:
+    file.write(updated_allow_list)
